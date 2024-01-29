a364ec9819cd62ad81afdca754a9527e51a1c064
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-01-29 14:24:53 -0300

    adicionando o container e o header a pagina principal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Container from "@/components/Container";
+import Header from "@/components/Header";
 
 export default function Home() {
   return <div>
-    <h1>Hello</h1>
+    <Header />
+    <Container>
+      <h1>Hello</h1>
+    </Container>
   </div>;
 }
